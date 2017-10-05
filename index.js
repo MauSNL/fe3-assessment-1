558d5150d8b16bd99a49e8ce1786947240fc8aa4
Mauro Stuifzand <maurostuif@hotmail.com>
2017-10-05 22:40:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//laad de data in van data.tsv file.
+d3.tsv("data.tsv", function(d) {
+  d.speakers = +d.speakers;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  x.domain(data.map(function(d) { return d.languages; }));
+  y.domain([0, d3.max(data, function(d) { return d.speakers; })]);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10, "%"))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("langue");
+
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.languages); })
+      .attr("y", function(d) { return y(d.speakers); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.speakers); });
+});

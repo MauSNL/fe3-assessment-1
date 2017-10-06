093ff549d1387e1981f8dc1eb0fde7a46547e198
Mauro Stuifzand <maurostuif@hotmail.com>
2017-10-06 15:07:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//Geeft waardes mee aan de SVG in de HTML.
 var svg = d3.select("svg"),
     margin = {top: 40, right: 40, bottom: 90, left: 80},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
-
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+    
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1), //Helpt het genereren van de bars.
+    y = d3.scaleLinear().rangeRound([height, 0]); //Helpt met het aangeven van de lengte.
 
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -19,14 +20,16 @@ d3.tsv("data.tsv", function(d) {
   x.domain(data.map(function(d) { return d.languages; }));
   y.domain([0, d3.max(data, function(d) { return d.speakers; })]);
 
+    //Genereerd de x axis.
   g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
+    //Genereerd de y axis.
   g.append("g")
       .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10))
+      .call(d3.axisLeft(y).ticks(10)) //Geeft de hoevelheid intervals aan.
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
